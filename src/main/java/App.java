46a8b971769ec99a27ffb2cae772bb8d17be483a
Vladyslav Malovanyi <sky0nf1re.bc@icloud.com>
2023-07-17 13:38:05 +0200

    hw.1.maven.v1.0

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+import dto.UserEntity;
+
+public class App {
+    public static void main(String[] args) {
+
+        UserEntity user=new UserEntity("Vladyslav", "Malovanyi");
+        System.out.println(user);
+    }
+}
---
+++ src/main/java/dto/UserEntity.java
@@ -0,0 +1,18 @@
+package dto;
+
+import com.google.gson.Gson;
+
+public class UserEntity {
+    private String firsName;
+    private String lastName;
+
+    public UserEntity(String firsName,String lastName){
+        this.firsName=firsName;
+        this.lastName=lastName;
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
